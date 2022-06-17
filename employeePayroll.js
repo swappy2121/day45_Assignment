f5c86ee73642bd91ddbc3c29283b49c784ff78ea
swapnil <sopiswarrinku77@gmail.com>
2022-06-16 23:45:02 -0700

    Added : edited the data and show data in table by using JSON

--- employeePayroll.js
+++ employeePayroll.js
@@ -2,22 +2,20 @@ window.addEventListener('DOMContentLoaded',(event)=>{
     creatInnerHtml();
 }) 
 
+
 const creatInnerHtml = () => {
-    const innerHtml =`
-    <tr>
-    <th>profile</th>
-    <th>Name</th>
-    <th>Gender</th>
-    <th>Department</th>
-    <th>Salary</th>
-    <th>Start Date</th>
-    <th>Actions</th>
-</tr>
+    const headerHtml =   `<th>profile</th><th>Name</th><th>Gender</th><th>Department</th><th>Salary</th><th>Start Date</th><th>Actions</th>
+    "`;
+let empPayrollData = createEmployeePayrollJSON()[0];
+
+    const innerHtml = 
+` ${headerHtml}
+
 <tr>
     <td><img  class="profile"   src="swapnil.jpeg" alt=""> 
     </td>
-    <td>SWAPNIL PAWAR</td>
-    <td>MALE</td>
+    <td>${empPayrollData._name}</td>
+    <td>${empPayrollData._gender}</td>
     <td><div class="dept-lavel">ENGINEERS</div>
     </td>
      <td>3000000</td>
@@ -27,7 +25,32 @@ const creatInnerHtml = () => {
             <img id="1" onclick="update(this)" alt="edit" src="editLogo.jpg" >
          </td>
 </tr>
-`
+`;
 document.querySelector('#display').innerHTML = innerHtml;
-
 }
+
+const createEmployeePayrollJSON = ()=>{ 
+		let empPayrollListLocal = [
+			{
+				_name: 'AVINASH PANDAV',
+				_gender: 'MALE',
+				_deptartment: 'Engineer',
+				_salary: '50000',
+				_startDate: '26 May 2020',
+				_note: ' ',
+				_id: new Date().getTime(),
+				_profilePic: 'swapnil.jpeg'
+			},
+			{
+				_name: 'AKSHAY PAWAR',
+				_gender: 'MALE',
+				_deptartment: 'MARKETING',
+				_salary: '100000',
+				_startDate: '20 MAY 2021',
+				_note: ' ',
+				_id: new Date().getTime() + 1,
+				_profilePic: ' '
+			}
+		];
+    return empPayrollListLocal;
+		}
